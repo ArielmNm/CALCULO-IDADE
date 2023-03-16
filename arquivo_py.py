64ad073b569428909733df7f5a81d99862481238
unknown <arielmmnogueira@gmail.com>
2023-03-16 16:47:52 -0300

    linhas de códigos com as variações de dados de idade e o calculo pra idade.

---
+++ arquivo_py.py
@@ -0,0 +1,10 @@
+nascimento = 2007
+anoAtual = 2023
+nome = 'Ariel'
+def calculoIdade(nascimento,anoAtual):
+    idade = anoAtual - nascimento
+    return idade
+idade = calculoIdade(nascimento,anoAtual)
+print(nome)
+print('IDADE É')
+print(idade)
